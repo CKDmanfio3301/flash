43814679ece45df53187cd3572578c4e4600eede
MANFIO3301 <xbieldsmx@gmail.com>
2024-11-22 11:35:23 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    '',
+    'Quem foi o primeiro presidente do Brasil?',
+    'marechal Manuel Deodoro da Fonseca'
+)
+
+criaCartao(
+    '',
+    'Quando o brasil foi descoberto pelos portugueses?',
+    '1500'
+)
+
+criaCartao(
+    '',
+    'Qual a capital do Brasil?',
+    'Brasília'
+)
+
+criaCartao(
+    '',
+    'Qual a cidade mais alta do Brasil?',
+    'Campos do Jordão'
+)
